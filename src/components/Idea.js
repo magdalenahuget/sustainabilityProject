f6ea3b9f85caa6e84b52f1e270b9c078ff301c78
magdalenahuget <magdalena.huget@gmail.com>
2023-12-02 20:13:01 +0100

    Add basic components

---
+++ src/components/Idea.js
@@ -0,0 +1,14 @@
+const Idea = ({ img, title, description }) => {
+    return (
+        <div className="idea">
+            <img className="idea-img" src={img} alt="{title}" />
+            <div className="idea-content">
+                <h1 className="idea-title">{title}</h1>
+                <p className="idea-description">{description}</p>
+            </div>
+            <button type="submit" className="idea-btn">
+                Discover more!
+            </button>
+        </div>
+    );
+};
