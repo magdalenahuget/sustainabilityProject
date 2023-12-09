40c02e6c85a43e44222e3a6280fbd1ca63833ee3
magdalenahuget <magdalena.huget@gmail.com>
2023-12-09 23:35:08 +0100

    Add and fix paragraph tags and data display

--- src/components/modal/ModalDiscoverMore.js
+++ src/components/modal/ModalDiscoverMore.js
@@ -3,17 +3,18 @@ import "react-pure-modal/dist/react-pure-modal.min.css";
 import PureModal from "react-pure-modal";
 import './Modal.css';
 
-function ModalDiscoverMore( props ) {
+function ModalDiscoverMore(props) {
     return (<>
         <PureModal className="ModalDiscoverMore"
-                   shouldCloseOnEsc = {true}
+                   shouldCloseOnEsc={true}
                    isOpen={props.open}
-                   header="Congrats!"
+                   header="Try this!"
                    footer={props.data}
                    onClose={props.close}
         >
-            <p style={{'word-break': 'normal'}}>Thanks to this solution in one month we use less plastic up to</p>
+            <p style={{wordBreak: 'normal'}}>Thanks to this solution in one month we use less plastic up to</p>
         </PureModal>
     </>)
 }
+
 export default ModalDiscoverMore
